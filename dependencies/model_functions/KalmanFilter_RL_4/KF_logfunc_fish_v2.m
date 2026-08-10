fe4d375a56802c3cdf25ca64aaf5f8943c28848e
Amber McLaughlin <147632563+amemc3@users.noreply.github.com>
2026-08-10 08:16:20 -0400

    Add files via upload

---
+++ dependencies/model_functions/KalmanFilter_RL_4/KF_logfunc_fish_v2.m
@@ -0,0 +1,153 @@
+function total_log = KF_logfunc_fish_v2(v, s, tau, data)
+
+%% Preallocate data structures
+nBlocks = 10;
+nTrials = 15;
+
+% Initialize total log
+total_log = 0;
+
+% Total reward
+total_reward = 100;
+%% for loop
+for iB = 1:nBlocks
+    % Initialize probability vector
+    prob_vector = zeros(nTrials,3);
+    prob_vector(1,:) = (ones(1,3))/3;
+    % Initialize expected reward vector
+    expected_reward = zeros(nTrials,3);
+    expected_reward(1,:) = (ones(1,3)/3)*100;
+    % Initialize log vector
+    log_value = zeros(nTrials, 1);
+    log_value(1, 1) = abs(log(1/3));
+    % Initialize variance vector
+    var_vector = zeros(nTrials,3);
+    var_vector(1,:) = .5;
+    % Initialize k vector
+    k = zeros(nTrials,3);
+    k(1,:) = .5;
+    % Initialize tallies
+    % Reward tallies
+    T1 = zeros(1,3);
+    T2 = zeros(1,3);
+    % Arm tally
+    A = zeros(1,3);
+    % Initialize expected values
+    E1 = zeros(1,3);
+    E2 = zeros(1,3);
+    % Stastistics
+    sigma = zeros(1,3);
+
+    for iT = 1:nTrials
+        % Extract user choice current
+        user_choice_current = data{iB,1}(iT,1);
+        if iT ~= 1
+            % Extract user choice past
+            user_choice = data{iB,1}(iT-1,1);
+            % Extract reward
+            if data{iB,1}(iT,5) == user_choice
+                reward = 100;
+            elseif data{iB,1}(iT, 5) ~= user_choice
+                reward = 0;
+            end
+        end
+
+        % Calculate sigma for kf
+        % Create tallies
+        % Reward tallies
+        if iT ~= 1
+            if user_choice_current ~= 0
+                if reward == 100
+                    T1(user_choice_current) = T1(user_choice_current) +1;
+                elseif reward == 0
+                    T2(user_choice_current) = T2(user_choice_current) +1;
+                end
+            end
+            % Arm tallies
+            if user_choice_current == 1
+                A(1) = A(1) + 1;
+            elseif user_choice_current == 2
+                A(2) = A(2) +1 ;
+            elseif user_choice_current == 3
+                A(3) = A(3) + 1;
+            elseif user_choice_current == 0
+                continue;
+            end
+
+            % Calculate E
+            for i = 1:3
+                if A(i) > 0
+                    E1(i) = T1(i)/A(i);
+                    E2(i) = T2(i)/A(i);
+                end
+            end
+
+            % Calculate Sigma [E(x^2) - (E(x))^2]]
+            for i = 1:3
+                if A(i) > 0
+                    % Weighted variance
+                    sigma(i) = ((1^2)*E1(i)+(2^2)*E2(i)) - ((1)*E1(i)+(2)*E2(i))^2;
+                else
+                    sigma(i) = .5;
+                end
+                % Sigma bounds
+                sigma(i) = max(0.2, min(sigma(i), 0.8));
+            end
+
+            % Add a "forgetting" drift if no option is selected
+            if iT ~= 1 && user_choice == 0
+                % Average prior probabilities with initials
+                prob_vector(iT,:) = (prob_vector(iT-1,:) + 1/3)/2;
+                % Notify
+                % disp(subject_list(iS).name);
+                % disp('failed to press button');
+            elseif iT ~= 1 && user_choice ~= 0
+
+                % setdiff
+                other_choices = setdiff(1:3,user_choice);
+
+                % Kalman gain
+                k(iT,:) = var_vector(iT-1, :)./(var_vector(iT-1, :) + sigma);
+
+                % update reward
+                expected_reward(iT, user_choice) = expected_reward(iT-1, user_choice) + s*k(iT,user_choice).*(reward - expected_reward(iT-1, user_choice));
+                expected_reward(iT, other_choices) = expected_reward(iT-1, other_choices) + s*k(iT,other_choices).*((total_reward - reward)/2 - expected_reward(iT-1, other_choices));
+
+                % update variance
+                var_vector(iT, :) = (1-k(iT,:)).*(var_vector(iT-1,:)) + v; %%% REVISIT Index two steps back
+
+                % softmax
+                prob_vector(iT,:)=(exp(expected_reward(iT,:)/tau))/sum(exp(expected_reward(iT,:)/tau));
+                % prob_vector(iT,:) = expected_reward(iT,:)/sum(expected_reward(iT,:));
+
+                % Add a minimum to the probability distribution to prevent
+                % optimization methods from getting stuck
+                prob_vector(iT,:) = max(0, min(prob_vector(iT,:), 1));
+                if any(prob_vector(iT, :) < 0.05)
+                    prob_vector(iT,:) = max(0.05, prob_vector(iT,:));
+                end
+                prob_vector(iT,:) = prob_vector(iT,:) / sum(prob_vector(iT,:));
+
+                % Calculate error continued
+                if user_choice_current ~= 0
+                    log_value(iT, 1) = abs(log(prob_vector(iT, user_choice_current)));
+                elseif user_choice_current == 0
+                    log_value(iT, 1) = 0;
+                end
+            end
+
+            % Store probabilities
+            prob_array{iB, 1}(iT,:) = prob_vector(iT,:);
+            % Store choices
+            choice_array{iB,1}(iT,1) = user_choice_current;
+            if iT ~= 1
+                % Store rewards
+                reward_array{iB,1}(iT,1) = reward;
+            end
+            % Store log(prob)
+            log_prob{iB,1}(iT,1) = log_value(iT,1);
+            % Total log
+            total_log = total_log + log_value(iT,1);
+        end
+    end
+end
---
+++ dependencies/model_functions/KalmanFilter_RL_4/PCB_Model_Wrapper_RL_KF1.m
@@ -0,0 +1,78 @@
+%% PCB_Model_Wrapper_RL_KF1
+% this is the wrapper function which runs the 
+% kalman filter RL model (RL_KF1) for a given task
+
+function PCB_Model_Wrapper_RL_KF1(data_folder, root_folder, day, group, task)
+
+cd (data_folder)
+
+% structure with .mat file information
+subdirs = dir('*.mat');
+% number of subjects
+n_subjects=length(subdirs);
+
+upperbound=100; % max values for v, s, and tau      
+lowerbound = 0; % min values 
+
+par_names{1}='v';
+
+prmtn=length(par_names);
+
+%% preallocate
+subj = cell(n_subjects,1);
+best_set = nan(n_subjects,prmtn*2+2);
+IDs=cell(n_subjects,1);
+all_data=cell(n_subjects,1);
+all_t_data=cell(n_subjects,1);
+all_cond3=cell(n_subjects,1);
+
+for i=1:n_subjects 
+    subj{i,1} = subdirs(i).name;
+end
+
+tic
+for agent=1:n_subjects
+    cd (data_folder)
+    clear data
+    clear t_data
+    clear cond2
+    disp(agent);
+    toc
+    %load data series per each agent
+    % cond is a matlab function - need to load DATA so we can interact with
+    % cond as a structure variable - AEM
+    DATA = load(subj{agent});
+    IDs{agent,1}=subj{agent}(1:(end-4));
+
+    %load real choice selections
+    all_data{agent,1}=DATA.data;
+    all_t_data{agent,1}=DATA.t_data;
+    all_cond3{agent,1}=DATA.cond;
+    
+    % Create the dynamic function name
+    funcName = ['PCB_fmin_function_', task, '_RL_KF1'];
+    
+    % Create function handle dynamically
+    fminsearch_function = str2func(funcName);
+    
+    % replaced with AEM version of bayes_function
+    [lambdalin,fvalin] = fminsearch(@(pars) fminsearch_function(pars, DATA.data, upperbound, lowerbound, 'lin'), 1);%[best_set(agent,1)]);
+    [lambdalog,fvalog] = fminsearch(@(pars) fminsearch_function(pars, DATA.data, upperbound, lowerbound, 'log'), 1);%[best_set(agent,3)]);
+
+    best_set(agent,1:prmtn)=lambdalin;
+    best_set(agent,prmtn+1)=fvalin;
+    best_set(agent,(prmtn+2):(prmtn*2+1))=lambdalog;
+    best_set(agent,prmtn*2+2)=fvalog;
+    
+    disp(best_set(agent,:));
+end
+
+mean_bs=mean(best_set(1:n_subjects,:));
+std_bs=std(best_set(1:n_subjects,:));
+
+cd(root_folder)
+filename = [day '_' task '_RL_KF1_' group '.mat'];
+save(filename, 'best_set', 'mean_bs', 'std_bs', 'par_names',...
+    'all_data', 'all_t_data', 'all_cond3', 'IDs');
+end
+
